0de85c4c9804b2a43ef97b27cc9f6656dbf244f4
André Mion <andremion@gmail.com>
2021-06-14 16:11:40 +0100

    Versioning project

---
+++ app/src/main/java/ch/protonmail/android/protonmailtest/MainActivity.kt
@@ -0,0 +1,23 @@
+package ch.protonmail.android.protonmailtest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongConstant")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pager = findViewById<ViewPager>(R.id.pager)
+        val adapter = TabsAdapter(this, supportFragmentManager)
+        pager.adapter = adapter
+    }
+
+    fun initTabs() {
+        //TODO
+    }
+}
